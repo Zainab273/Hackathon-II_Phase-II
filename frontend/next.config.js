e04a9eea380d8a2c82af9ef58eb24def4f224a0f
Zainab273 <syedazainabrashid653@gmail.com>
2026-02-09 03:22:47 +0500

    first commit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.alias['@'] = require('path').join(__dirname, 'src');
+    return config;
+  },
 }
 
 module.exports = nextConfig
